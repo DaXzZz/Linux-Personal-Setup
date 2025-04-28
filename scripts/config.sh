201c05c622c19ca392bd8b8e2435de7c046cddfd
Nontapat Chucharnchai <ryurock1011@gmail.com>
2025-04-29 03:03:18 +0700

    Update Config File

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,4 +1,3 @@
-# config.sh
 #!/bin/bash
 
 set -euo pipefail
@@ -15,19 +14,23 @@ done
 FILES_TO_MANAGE=(
   "/etc/default/grub"
   "${HOME}/.zshrc"
-  "${HOME}/.config/kitty/kitty.conf"
   "${HOME}/.config/starship.toml"
+  "${HOME}/.config/kitty/kitty.conf"
+  "${HOME}/.config/fastfetch/config.jsonc"
   "${HOME}/.config/hypr/UserConfigs/WindowRules.conf"
   "${HOME}/.config/hypr/UserConfigs/Startup_Apps.conf"
   "${HOME}/.config/hypr/UserConfigs/UserSettings.conf"
+  "${HOME}/.config/hypr/UserConfigs/UserDecorations.conf"
 )
 
 declare -A RESTORE_PATHS=(
   [grub]="/etc/default/grub"
   [zshrc]="${HOME}/.zshrc"
-  [kitty.conf]="${HOME}/.config/kitty/kitty.conf"
   [starship.toml]="${HOME}/.config/starship.toml"
+  [kitty.conf]="${HOME}/.config/kitty/kitty.conf"
+  [config.jsonc]="${HOME}/.config/fastfetch/config.jsonc"
   [WindowRules.conf]="${HOME}/.config/hypr/UserConfigs/WindowRules.conf"
   [Startup_Apps.conf]="${HOME}/.config/hypr/UserConfigs/Startup_Apps.conf"
   [UserSettings.conf]="${HOME}/.config/hypr/UserConfigs/UserSettings.conf"
+  [UserDecorations.conf]="${HOME}/.config/hypr/UserConfigs/UserDecorations.conf"
 )
